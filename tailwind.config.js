8999d26e27d57cf30e441ce32ef323b0419276a6
shripal <shripalp@gmail.com>
2023-10-26 14:07:06 -0300

    tailwind js fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js",
-    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
