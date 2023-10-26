6f973de69b9d96542a818086790691d7594015f2
shripal <shripalp@gmail.com>
2023-10-26 14:13:42 -0300

    tailwind js fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,8 @@ import flowbitePlugin from "flowbite/plugin";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
+
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
