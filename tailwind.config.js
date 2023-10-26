1fcfff30fa9d5a4b4855e87194a1780390a3674d
shripal <shripalp@gmail.com>
2023-10-26 13:57:24 -0300

    xided

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+import flowbitePlugin from "flowbite/plugin";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
@@ -14,5 +16,5 @@ export default {
     },
   },
   // eslint-disable-next-line no-undef
-  plugins: [require("flowbite/plugin")],
+  plugins: [flowbitePlugin],
 };
